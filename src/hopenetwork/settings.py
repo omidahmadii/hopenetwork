a01da90e8e8ad4ca3f798568595dd451c55da2b6
Omid Ahmadi <o.ahmadi1992@gmail.com>
2022-03-13 22:43:00 +0330

    init

--- src/hopenetwork/settings.py
+++ src/hopenetwork/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / '../site/public/static/'
+#STATIC_ROOT = BASE_DIR / '../site/public/static/'
+STATIC_ROOT = '/django-project/hopenetwork/site/public/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
